e3f35ee278604ddb269ddb36789e94ad329a4a60
duytran92 <superduy.bkict@gmail.com>
2012-04-10 16:38:20 +0700

    Update document.cpp

--- document.cpp
+++ document.cpp
@@ -1,29 +1,38 @@
 #include "document.h"
 
-Document::Document(string)
+Document::Document()
 {
+    name_ = "";
+    occurence_ = 0;
+}
+
+Document::Document(string name)
+{
+    name_ = name;
+    occurence_ = 1;
 }
 
 string Document::name()
 {
+    return name_;
 }
 
-void Document::name(string docname)
+void Document::name(string name)
 {
+    name_ = name;
 }
 
 int Document::occurrence()
 {
+    return occurrence_;
 }
 
-void Document::occurrence(int n)
+void Document::occurrence(int occurence)
 {
+    occurrence_ = occurrence;
 }
 
 void Document::increaseOccurrence(int n)
 {
-}
-
-Document::Document()
-{
+    occurrence_ += n;
 }
