e22d74b0860458b4014495e33544ea75d0b755ea
duytran92 <superduy.bkict@gmail.com>
2012-04-10 16:46:35 +0700

    Update inodedata.h

--- inodedata.h
+++ inodedata.h
@@ -9,12 +9,14 @@ private:
 
 public:
     INodeData();
+    INodeData(string word);
     string word();
     void word(string name);
     vector<Document> docs();
     void docs(Document doc);
     vector<Document> conjunt(INodeData * node_data);
     vector<Document> compose(INodeData * node_data);
+    void sortDocs();
 };
 
 #endif // INODEDATA_H
