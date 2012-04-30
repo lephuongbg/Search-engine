e55aaf3105bef877bc3b5e4e532c1676d1046c46
Lê Hoàng Phương <herophuong93@gmail.com>
2012-04-30 22:50:42 +0700

    significantly speed optimize for large data set

--- inodedata.cpp
+++ inodedata.cpp
@@ -37,13 +37,13 @@ vector<Document> INodeData::docs()
 void INodeData::docs(Document doc)
 {
     //Check for existance of the document in the list
-    for (vector<Document>::iterator it = docs_.begin(); it != docs_.end(); it++)
+    for (vector<Document>::reverse_iterator rit = docs_.rbegin(); rit != docs_.rend(); rit++)
     {
         //If yes,
-        if (it->name_ == doc.name_)
+        if (rit->name_ == doc.name_)
         {
             //Increase the word occurrence counter for the document
-            it->occurrence_ += doc.occurrence_;
+            rit->occurrence_ += doc.occurrence_;
             return;
         }
     }
